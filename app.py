510ce1c125e358c717ab54a5f4cd3df1ee20539d
Victor <victor3790@gmail.com>
2026-03-14 11:27:11 -0600

    Flash message shown

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = "test1"
 
 @app.route("/hello")
 def index():
+    flash("This is a flash message!")
     return render_template("index.html")
