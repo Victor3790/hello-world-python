2df56ab52eba781c0119f6e35674251ad9887741
Victor <victor3790@gmail.com>
2026-03-16 19:24:01 -0600

    Index route added

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 app.secret_key = "test1"
 
-@app.route("/hello")
-def index():
-    flash("This is a flash message!")
+@app.route("/")
+def home():
     return render_template("index.html")
+
+@app.route("/hello")
+def hello():
+    flash("This is a flash message!")
+    return render_template("hello.html")
